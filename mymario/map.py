e9182579a8819823f813934d5c7a9ad9f34c6739
Sriteja Sugoor <sritejakittu777@gmail.com>
2018-12-22 17:52:30 +0530

    Removed unnecessary methods and added method to view map.

--- mymario/map.py
+++ mymario/map.py
@@ -3,13 +3,15 @@ A module containing class for map containing all objects
 """
 
 import config
+import os
+from time import sleep
 
 
 class Map:
     """
     A Generalised class for all level maps
     """
-    def __init__(self, columns, rows, map_length):
+    def __init__(self, columns, rows, map_length, player_position):
         """
         Initialises the attributes of map
         """
@@ -18,28 +20,36 @@ class Map:
         self.columns = columns
         self.string_array = [[' ' for _ in range(1, columns + 1)] for _ in range(1, rows + 1)]
         self.object_array = [[0 for _ in range(1, columns + 1)] for _ in range(1, rows + 1)]
-        self._left_pointer = [0, ]
-        self._right_pointer = [config.COLUMNS, ]
-        self.player = ''
+        self.foreground = []
+        self.background = []
         self.checkpoints = []
         self.enemies = []
         self.bridges = []
         self.holes = []
         self.coins = []
         self.control_music = ''
+        self.initial_position = player_position
 
-    def increment_left_pointer(self, value=1):
-        """
-        Increments map left pointer by value
-        :param value: value by which left pointer has to be incremented default value 1
-        :return:
-        """
-        self._left_pointer += value
+    def view_map(self, reverse=False):
+        flag = 1
+        lp = 0
+        rp = self.columns
 
-    def increment_right_pointer(self, value=1):
-        """
-        Increments map right pointer by value
-        :param value: value by which right pointer has to be incremented default value 1
-        :return:
-        """
-        self._right_pointer += value
+        if reverse:
+            flag = -1
+            rp = self.length
+            lp = self.length - self.columns
+
+        while True:
+            os.system('tput reset')
+            for i in config.DIMENSIONAL_ARRAY[:]:
+                for j in i[lp:rp]:
+                    print(j, end='')
+                print()
+                print('\r', end='')
+            if rp > self.length or lp < 0:
+                break
+
+            lp += flag
+            rp += flag
+            sleep(0.05)
