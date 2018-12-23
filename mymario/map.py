f39be55174e86dc9ab45155d0020b2346273712d
Sriteja Sugoor <sritejakittu777@gmail.com>
2018-12-23 13:43:39 +0530

    Changed the array length to map length

--- mymario/map.py
+++ mymario/map.py
@@ -11,15 +11,15 @@ class Map:
     """
     A Generalised class for all level maps
     """
-    def __init__(self, columns, rows, map_length, player_position):
+    def __init__(self, columns, rows, map_length):
         """
         Initialises the attributes of map
         """
         self.length = map_length
         self.rows = rows
         self.columns = columns
-        self.string_array = [[' ' for _ in range(1, columns + 1)] for _ in range(1, rows + 1)]
-        self.object_array = [[0 for _ in range(1, columns + 1)] for _ in range(1, rows + 1)]
+        self.map_array = [[' ' for _ in range(1, self.length + 1)] for _ in range(1, self.rows + 1)]
+        self.object_array = [[0 for _ in range(1, self.length + 1)] for _ in range(1, self.rows + 1)]
         self.foreground = []
         self.background = []
         self.checkpoints = []
@@ -28,7 +28,6 @@ class Map:
         self.holes = []
         self.coins = []
         self.control_music = ''
-        self.initial_position = player_position
 
     def view_map(self, reverse=False):
         flag = 1
@@ -40,9 +39,15 @@ class Map:
             rp = self.length
             lp = self.length - self.columns
 
+        # for i in self.map_array[:]:
+        #     for j in i[lp:rp]:
+        #         print(j, end='')
+        #     print()
+        #     print('\r', end='')
+
         while True:
             os.system('tput reset')
-            for i in config.DIMENSIONAL_ARRAY[:]:
+            for i in self.map_array[:]:
                 for j in i[lp:rp]:
                     print(j, end='')
                 print()
@@ -53,3 +58,5 @@ class Map:
             lp += flag
             rp += flag
             sleep(0.05)
+
+    # def remove_obj(self, ):
