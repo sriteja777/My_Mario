1ec1ab25e3b4d27e8e11a0dc50893f4d9172a3aa
Sriteja Sugoor <sritejakittu777@gmail.com>
2018-12-23 20:50:19 +0530

    Added input controls for multi player

--- mymario/game2.py
+++ mymario/game2.py
@@ -1,7 +1,14 @@
 import os
+from random import randrange
+from threading import Thread
+from time import sleep
+
 import level1_map
 import config
 from motion import Player
+from objects import Obj
+
+inc = 0
 
 
 class Game:
@@ -12,6 +19,7 @@ class Game:
         if not self.multi_player_support(nop):
             print("Sorry " + str(nop) + " is not supported at present screen aspect ratio.")
             return
+        self.initialise_screen()
         self.num_players = nop
         self.left_pointer = []
         self.right_pointer = []
@@ -21,8 +29,24 @@ class Game:
         for _ in range(nop):
             self.maps.append(level1_map.Level1Map(self.screen['rows'], int(self.screen['columns']/nop - 1)))
             self.players.append(Player(self.maps[-1].initial_player_position, config.PLAYER, self.maps[-1].map_array))
+            self.left_pointer.append(0)
+            self.right_pointer.append(self.maps[-1].columns)
         # self.maps[0].view_map()
         self.print_screen()
+        inp_thread = Thread(target=self.get_input)
+        inp_thread.daemon = True
+        inp_thread.start()
+        while True:
+            try:
+                if config.stop.is_set() or config.timeout.is_set():
+                    break
+                sleep(0.1)
+                self.make_updates(0)
+                self.make_updates(1)
+                self.updates()
+
+            except KeyboardInterrupt:
+                break
 
     def multi_player_support(self, num_of_players):
         """
@@ -37,6 +61,9 @@ class Game:
             return True
         else:
             return False
+
+    def initialise_screen(self):
+        Obj(self.screen['columns'], self.screen['rows'], 1, 3, ' ')
 
     def print_screen(self):
         lp = 0
@@ -56,4 +83,98 @@ class Game:
         return int(rows) - 3, int(columns)
 
 
-game = Game(3)
+    def get_input(self):
+        """
+        Get input from the user and perform corresponding actions in the game
+        :return:
+        """
+        getch = config.getch_unix
+        while True:
+            k = getch()
+            if k == 'q':
+                config.stop.set()
+                break
+
+            # if k == ' ':
+            #     # pause the game
+            #     c.CONTROL_MUSIC[0].play_music_for_action('Game paused')
+            #     if c.pause.is_set():
+            #         c.pause.clear()
+            #     else:
+            #         c.pause.set()
+
+            if self.players[0].is_alive and not config.pause.is_set() and not config.timeout.is_set():
+                if k == 'a':
+                    self.players[0].move_left()
+                elif k == 'd':
+                    self.players[0].move_right()
+                elif k == 'w':
+                    self.players[0].move_up()
+
+                elif k == 'e':
+                    self.players[0].move_up_right()
+                elif k == 'z':
+                    self.players[0].move_up_left()
+                elif k == 'f':
+                    # launch_stones()
+                    pass
+                if k == 'j':
+                    self.players[1].move_left()
+                elif k == 'l':
+                    self.players[1].move_right()
+                elif k == 'i':
+                    self.players[1].move_up()
+
+                elif k == 'o':
+                    self.players[1].move_up_right()
+                elif k == 'm':
+                    self.players[1].move_up_left()
+            if config.timeout.is_set():
+                break
+            if config.stop.is_set():
+                break
+
+    def make_updates(self, x):
+        """
+        Make updates and prints the map to the terminal
+        :return:
+        """
+        global inc
+
+        rand_x = randrange(1, 100)
+        rand_x_2 = randrange(1, 150)
+        rand_x_3 = randrange(1, 75)
+        if inc % rand_x == 0:
+            self.maps[x].clouds[0].move_cloud()
+        if inc % rand_x_2 == 0:
+            self.maps[x].clouds[1].move_cloud()
+        if inc % rand_x_3 == 0:
+            self.maps[x].clouds[2].move_cloud()
+        inc += 1
+
+    def updates(self):
+        os.system('tput reset')
+        fourth = int(self.screen['columns'] / 4) - 8
+        combined_list = list([self.maps[x].map_array for x in range(self.num_players)])
+        for i in zip(*combined_list):
+            for j, item in enumerate(i):
+                for k in item[self.left_pointer[j]:self.right_pointer[j]]:
+                    print(k, end='')
+                print(config.LINE, end='')
+            print()
+            print('\r', end='')
+
+        # for i in self.maps[x].map_array[:]:
+        #     for j in i[self.left_pointer[x]:self.right_pointer[x]]:
+        #         print(j, end='')
+        #     print()
+        #     print('\r', end='')
+        # print('\r' + config.SCORE_TITLE + ": " + str(self.players[x].score),
+        #       config.TIME + ': ' + str(self.players[x].time), config.LEVEL_I_TITLE,
+        #       config.STONE + ' * ' + str(self.players[x].stones),
+        #       config.LOVE + ' *' + str(self.players[x].get_lives()),
+        #       sep=' ' * fourth)
+        # print('\r' + ' ' * config.SPACES_BEFORE_TITLE, config.TITLE)
+
+
+game = Game(2)
