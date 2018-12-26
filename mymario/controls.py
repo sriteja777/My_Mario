db541d10372329cc6b148698062fb4d535392237
Sriteja Sugoor <sritejakittu777@gmail.com>
2018-12-27 02:46:13 +0530

    Added controls for upto 4 players including bullets

--- mymario/controls.py
+++ mymario/controls.py
@@ -1,19 +1,24 @@
+
+
 class ObjectSetup(object):
     pass
 
 
-def set_keys_for_player(up='', left='', right='', up_right='', up_left=''):
+def set_keys_for_player(up=None, left=None, right=None, up_right=None, up_left=None, bullet=''):
     keys = ObjectSetup()
     keys.UP = up
     keys.LEFT = left
     keys.RIGHT = right
     keys.UP_RIGHT = up_right
     keys.UP_LEFT = up_left
+    keys.BULLET = bullet
     return keys
 
 
 class Controls:
     def __init__(self):
         self.player = []
-        self.player.append(set_keys_for_player('w', 'a', 'd', 'e', 'z'))
-        self.player.append(set_keys_for_player('i', 'j', 'l', 'o', 'm'))
+        self.player.append(set_keys_for_player('w', 'a', 'd', 'e', 'z', 's'))
+        self.player.append(set_keys_for_player('t', 'f', 'h', 'y', 'v', 'g'))
+        self.player.append(set_keys_for_player('i', 'j', 'l', 'o', 'm', 'k'))
+        self.player.append(set_keys_for_player('8', '4', '6', '9', '1', '5'))
